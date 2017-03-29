214d4f12bb4f70c8953a2b939719d381792f8071
mwolcott2 <mwolcott2@unm.edu>
2017-03-29 16:44:48 -0600

    Added twoSpecies boolean in Main, press P to pause/unpause, slowed the animation, changed fire from red to orange per the assignment spec.

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,7 @@ public class Main extends Application implements EventHandler<KeyEvent>
   private final double CAMERA_NEAR_CLIP = 0.1;
   private final double CAMERA_FAR_CLIP = 10000.0;
   private final double LIGHTNING_STRIKE_PROB = 0.001;
+  private boolean twoSpecies = false;
 
   static Cell[][] graph = new Cell[252][252]; //252 for padding
   private int[][] nextState = new int[252][252]; //252 to match the graphs size, makes the logic simpler to understand.
@@ -61,7 +62,7 @@ public class Main extends Application implements EventHandler<KeyEvent>
     topScene.setOnKeyPressed(this);
 
     SubScene scene = new SubScene(root, 768, 768, true, SceneAntialiasing.BALANCED);
-    scene.setFill(Color.GREY);
+    scene.setFill(Color.BLACK);
 
     pane.setCenter(scene);
 
@@ -77,6 +78,7 @@ public class Main extends Application implements EventHandler<KeyEvent>
     scene.setCamera(camera);
 
     loop.start();
+    loop.running = true;
   }
 
   /**
@@ -85,9 +87,9 @@ public class Main extends Application implements EventHandler<KeyEvent>
    */
   private void buildBoard()
   {
-    for (int i = 1; i <= 251; i++)
+    for(int i = 1; i <= 251; i++)
     {
-      for (int j = 1; j <= 251; j++)
+      for(int j = 1; j <= 251; j++)
       {
         nextState[i][j] = 0;
         graph[i][j] = new Cell(i, j, 0);
@@ -99,11 +101,11 @@ public class Main extends Application implements EventHandler<KeyEvent>
     //graphXForm.rx.setAngle(cameraXform.rx.getAngle());
     //graphXForm.ry.setAngle(cameraXform.ry.getAngle());
 
-    for (int i = 1; i <= 251; i++)
+    for(int i = 1; i <= 251; i++)
     {
-      for (int j = 1; j <= 251; j++)
+      for(int j = 1; j <= 251; j++)
       {
-        graph[i][j].setNeighbors(getNeighbors(i,j));
+        graph[i][j].setNeighbors(getNeighbors(i, j));
       }
     }
   }
@@ -112,13 +114,13 @@ public class Main extends Application implements EventHandler<KeyEvent>
   {
     List<Cell> neighbors = new ArrayList<>();
 
-    for(int row = i - 1; row <=  i + 1; row++)
+    for(int row = i - 1; row <= i + 1; row++)
     {
       for(int col = j - 1; col <= j + 1; col++)
       {
         if(row != col)
         {
-          if( row > 0 && col > 0 && row < 252 && col < 252)
+          if(row > 0 && col > 0 && row < 252 && col < 252)
           {
             neighbors.add(graph[row][col]);
           }
@@ -138,14 +140,28 @@ public class Main extends Application implements EventHandler<KeyEvent>
   @Override
   public void handle(KeyEvent e)
   {
-    if (e.getCode() == KeyCode.O)
+    if(e.getCode() == KeyCode.O)
     {
-     cameraDistance -= 2;
-     camera.setTranslateZ(cameraDistance);
-    } else if (e.getCode() == KeyCode.I)
+      cameraDistance -= 2;
+      camera.setTranslateZ(cameraDistance);
+    }
+    else if(e.getCode() == KeyCode.I)
     {
-     cameraDistance += 2;
-     camera.setTranslateZ(cameraDistance);
+      cameraDistance += 2;
+      camera.setTranslateZ(cameraDistance);
+    }
+    else if(e.getCode() == KeyCode.P)
+    {
+      if(loop.running)
+      {
+        loop.stop();
+        loop.running = false;
+      }
+      else
+      {
+        loop.start();
+        loop.running = true;
+      }
     }
   }
 
@@ -173,6 +189,8 @@ public class Main extends Application implements EventHandler<KeyEvent>
    */
   class Loop extends AnimationTimer
   {
+    int slow = 0;
+    boolean running = false;
     int frame = 0;
     private TreeSpecies one = new TreeSpecies(0.75, Color.FORESTGREEN);
     private TreeSpecies two = new TreeSpecies(0.5, Color.DARKOLIVEGREEN);
@@ -180,9 +198,14 @@ public class Main extends Application implements EventHandler<KeyEvent>
     @Override
     public void handle(long time)
     {
-      updateGraph();
-      //clearDeadCells();
-      frame++;
+      if(slow % 25 == 0)
+      {
+        slow = 1;
+        updateGraph();
+        //clearDeadCells();
+        frame++;
+      }
+      slow++;
     }
 
     private void createTwoSpecies()
@@ -194,9 +217,9 @@ public class Main extends Application implements EventHandler<KeyEvent>
     private void updateGraph()
     {
       //Updates the state based on the status
-      for (int i = 1; i <= 251; i++)
+      for(int i = 1; i <= 251; i++)
       {
-        for (int j = 1; j <= 251; j++)
+        for(int j = 1; j <= 251; j++)
         {
           int status = graph[i][j].getStatus();
           if(status == 3)
@@ -216,7 +239,7 @@ public class Main extends Application implements EventHandler<KeyEvent>
           {
             if(random.nextDouble() < LIGHTNING_STRIKE_PROB)
             {
-              nextState[i][j] = 2;
+              nextState[i][j] = 3;
             }
           }
           //if barren chance to spawn tree
@@ -226,25 +249,28 @@ public class Main extends Application implements EventHandler<KeyEvent>
             {
               nextState[i][j] = 1;
             }
-            else if(random.nextDouble() < two.getProbability())
+            else if(twoSpecies)
             {
-              nextState[i][j] = 2;
+              if(random.nextDouble() < two.getProbability())
+              {
+                nextState[i][j] = 2;
+              }
             }
           }
         }
       }
 
       //Updates cell only when needed
-      for (int i = 1; i <= 251; i++)
+      for(int i = 1; i <= 251; i++)
       {
-        for (int j = 1; j <= 251; j++)
+        for(int j = 1; j <= 251; j++)
         {
           int status = nextState[i][j];
-          if (graph[i][j].getStatus() != status)
+          if(graph[i][j].getStatus() != status)
           {
             if(status == 3)
             {
-              graph[i][j].getCell().setFill(Color.RED);
+              graph[i][j].getCell().setFill(Color.ORANGE);
               graph[i][j].getCell().setVisible(true);
               graph[i][j].setStatus(3);
             }
@@ -262,7 +288,7 @@ public class Main extends Application implements EventHandler<KeyEvent>
             }
             else if(status == 0)
             {
-              graph[i][j].getCell().setFill(Color.WHITE);
+//              graph[i][j].getCell().setFill(Color.WHITE);
               graph[i][j].getCell().setVisible(false);
               graph[i][j].setStatus(0);
             }
@@ -276,6 +302,9 @@ public class Main extends Application implements EventHandler<KeyEvent>
   /**
    * @param args the command line arguments
    */
-  public static void main(String[] args) { launch(args); }
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
 }
